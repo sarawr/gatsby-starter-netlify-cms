6819faba66155db16ee8d1b2b75fc193b4ff15a6
sarawr <hello@saraedin.se>
2018-05-11 23:29:39 +0200

    woop

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+
+const Footer = () => (
+  <footer className="footer is-transparent">
+        <div className="columns">
+          <div className="column is-8">
+	    <div className="footer-brand">
+
+    </div></div></div>
+  </footer>
+)
+
+export default Footer
