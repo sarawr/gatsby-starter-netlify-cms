4273b8e5701282df8c2dc7641ccf34b5e8e5e981
sarawr <hello@saraedin.se>
2018-05-16 22:42:34 +0200

    📝

--- src/templates/contact-page.js
+++ src/templates/contact-page.js
@@ -18,11 +18,31 @@ export const ContactPageTemplate = ({ title, content, contentComponent }) => {
               <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
                 {title}
               </h2>
+              <div className="contact-info">
+              <div className="form">
+              <form name="contact" method="POST" netlify>
+                <div className="field"><p>
+                  <label>Name <input type="text" class="input" name="name" /></label>
+                </p></div>
+                <div className="field"><p>
+                  <label>Email <input type="email" class="input" name="email" /></label>
+                </p></div>
+                <div className="field"><p>
+                <label>Message: <textarea class="textarea" name="message" /></label>
+              </p></div>
+                <p>
+                  <button className="button is-link" type="submit">Send</button>
+                </p>
+              </form>
+              </div>
               <PageContent className="content contact" content={content} />
+
+              </div>
               <div className="map" style={{ backgroundImage: `url(${map})` }}></div>
             </div>
           </div>
         </div>
+
          <div className="social-mobile">
           <a href="https://www.imdb.com/name/nm4323116/?ref_=nv_sr_1"><img src={imdb} style={{ width: '24px' }} /></a>
           <a href="https://www.linkedin.com/in/hugo-lundgren-50baa6b2/"><img src={linkedin} style={{ width: '24px' }} /></a>
