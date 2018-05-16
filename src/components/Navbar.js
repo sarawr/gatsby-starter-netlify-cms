72f22d6cad8ecfa9390fe55810ab06f34764ad7a
sarawr <hello@saraedin.se>
2018-05-16 22:12:10 +0200

    📱🙏

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,25 @@ import Link from 'gatsby-link'
 
 import linkedin from '../img/linkedin.svg'
 import imdb from '../img/imdb.svg'
+import menu from '../img/menu.svg'
 
-const Navbar = () => (
+export default class Navbar extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      toggle: false
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+  handleClick(){
+    this.setState({
+      toggle: !this.state.toggle,
+    })
+  }
+  render() {
+    return(
   <nav className="navbar is-transparent">
-    <div className="container">
+    <div className="container hide-mobile">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
           <strong>Hugo Lundgren</strong>
@@ -23,12 +38,31 @@ const Navbar = () => (
           Contact
         </Link>
       </div>
-      <div className="navbar-end hide-mobile">
+      <div className="navbar-end">
                 <div className="navbar-item"><a href="https://www.imdb.com/name/nm4323116/?ref_=nv_sr_1"><img src={imdb} style={{ width: '24px' }} /></a></div>
           <div className="navbar-item"><a href="https://www.linkedin.com/in/hugo-lundgren-50baa6b2/"><img src={linkedin} style={{ width: '24px' }} /></a></div>
       </div>
     </div>
-  </nav>
-)
 
-export default Navbar
+    <div className="container show-mobile">
+      <div onClick={this.handleClick} className="navbar-brand">
+          <strong>Hugo Lundgren</strong><div className="navbar-item"><img src={menu} style={{ width: '24px' }} /></div>
+      </div>
+
+      {this.state.toggle ?
+      <div className="navbar-start">
+      <Link onClick={this.handleClick} className="navbar-item" to="/">
+          Work
+        </Link>
+        <Link onClick={this.handleClick} className="navbar-item" to="/about">
+          About
+        </Link>
+        <Link onClick={this.handleClick} className="navbar-item" to="/contact">
+          Contact
+        </Link>
+      </div> : '' }
+    </div>
+
+  </nav>
+)}}
+
