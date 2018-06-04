bdb5607f3964154a9491dfe83f83e31791ba053b
sarawr <hello@saraedin.se>
2018-06-04 14:45:15 +0900

    footer fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,9 @@ const Footer = () => (
   <footer className="footer is-transparent">
     <div className="hugo-footer">
 	   <div className="footer-brand">
-	   <div className="column">Gästrikegatan 9, 113 62 Stockholm</div>
-	   <div className="column">+46 708107289 <a href='"mailto:"hugo@hugolundgren.se"' style={{ textDecoration: 'underline' }} >hugo@hugolundgren.se</a></div>
-	   <div className="column"><a href="https://www.linkedin.com/in/hugo-lundgren-50baa6b2/">LinkedIn</a><br/><a href="https://www.imdb.com/name/nm4323116/?ref_=nv_sr_1">IMDB</a></div>
+	   <div className="fot">Gästrikegatan 9<br/> 113 62 Stockholm</div>
+	   <div className="fot">+46 708107289 <br/><a href='"mailto:"hugo@hugolundgren.se"' style={{ textDecoration: 'underline' }} >hugo@hugolundgren.se</a></div>
+	   <div className="fot"><a href="https://www.linkedin.com/in/hugo-lundgren-50baa6b2/">LinkedIn</a><br/><a href="https://www.imdb.com/name/nm4323116/?ref_=nv_sr_1">IMDB</a></div>
        </div>
     </div>
   </footer>
