dc0ade6381581128209dd5c7748b0321fdfbd801
axelinternet <axel@hultman.info>
2019-03-18 23:12:05 +0100

    Changed height of iframe google map. Have no idea how this worked before or how it should look

--- src/templates/contact-page.js
+++ src/templates/contact-page.js
@@ -10,7 +10,7 @@ export const ContactPageTemplate = ({ title, content, contentComponent, mapTitle
   return (
     <Layout>
       <section className="section">
-        <div className="columns page">
+        <div className="columns page page--contact">
           <div className="column is-5">
             <div className="section">
               <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
@@ -23,10 +23,14 @@ export const ContactPageTemplate = ({ title, content, contentComponent, mapTitle
             </div>
           </div>
           <div className="column is-4">
-            <h3 className="title is-size-4 has-text-weight-bold is-bold-light hide-mobile">
-              {mapTitle}
-            </h3>
-            <iframe title="A map to find hugo" className="hide-mobile" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3326.7085286621664!2d18.037115285266104!3d59.34118398157301!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465f9d7a6bb45a65%3A0x72278647da323ea6!2sG%C3%A4strikegatan+9%2C+113+62+Stockholm!5e0!3m2!1sen!2sse!4v1527454291455&zoom=15" width="380" height="380" frameborder="0" allowfullscreen></iframe>
+            <div className="section">
+              <h3 className="title is-size-4 has-text-weight-bold is-bold-light hide-mobile">
+                {mapTitle}
+                <div className="section">              
+              <iframe title="A map to find hugo" className="hide-mobile" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3326.7085286621664!2d18.037115285266104!3d59.34118398157301!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x465f9d7a6bb45a65%3A0x72278647da323ea6!2sG%C3%A4strikegatan+9%2C+113+62+Stockholm!5e0!3m2!1sen!2sse!4v1527454291455&zoom=15" width="380" height="450" frameborder="0" allowfullscreen></iframe>
+              </div>
+              </h3>
+            </div>
           </div>
         </div>
       </section>
